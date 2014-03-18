fbf22e60d64c8a33977764c462120ea4bc7dd5c6
masih <masiih@gmail.com>
2014-03-18 14:56:04 +0000

    disabled quantiletest

--- src/test/java/org/mashti/sina/distribution/ZipfDistributionTest.java
+++ src/test/java/org/mashti/sina/distribution/ZipfDistributionTest.java
@@ -17,7 +17,6 @@
 
 package org.mashti.sina.distribution;
 
-import java.util.Random;
 import org.junit.Test;
 
 /** @author Masih Hajiarabderkani (mh638@st-andrews.ac.uk) */
@@ -35,13 +34,13 @@ public class ZipfDistributionTest {
 
     @Test
     public void testQuantile() throws Exception {
-
-        ProbabilityDistribution distribution = new ZipfDistribution(10, 1);
-        Random random = new Random(5421);
-        for (int i = 0; i < 100; i++) {
-
-            System.out.println(distribution.quantile(random.nextDouble()));
-        }
+        //
+        //        ProbabilityDistribution distribution = new ZipfDistribution(10, 1);
+        //        Random random = new Random(5421);
+        //        for (int i = 0; i < 100; i++) {
+        //
+        //            System.out.println(distribution.quantile(random.nextDouble()));
+        //        }
 
     }
 
